702924541ba587d646a5c472364271bfec9b7d57
Janani S <janani.st@prodapt.com>
2022-07-18 13:31:16 +0530

    Tester code

---
+++ SITester.java
@@ -0,0 +1,11 @@
+package com.prodapt.basics;
+
+public class SITester {
+
+	public static void main(String[] args) {
+		SimpleInterest si= new SimpleInterest();
+		si.simple_Interest(7000, 20, 2);
+
+	}
+
+}
